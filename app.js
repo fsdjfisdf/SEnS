1d578ca3c9d2ac719bbe54f26adadab198036b94
fsdjfisdf <165735907+fsdjfisdf@users.noreply.github.com>
2024-04-16 10:56:43 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/yourDatabase', {
+mongoose.connect('mongodb://localhost:27017/SEnS', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
