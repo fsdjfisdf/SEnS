6d11c5109d4e08c9b8f4c55bdb0b579eca9defbc
fsdjfisdf <165735907+fsdjfisdf@users.noreply.github.com>
2024-04-16 14:03:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -299,7 +299,7 @@ document.getElementById('saveButton').addEventListener('click', async function()
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(newWorkLog)
+            body: JSON.stringify(workLog)
         });
 
         if (!response.ok) throw new Error('Something went wrong');
